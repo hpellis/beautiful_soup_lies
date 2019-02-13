230fc3146a208c60dd4d50708fa196be6f2b2740
hpellis <harriet.p.ellis@gmail.com>
2019-02-13 11:52:19 +0000

    changing dict to list

--- trump_lies.py
+++ trump_lies.py
@@ -12,15 +12,23 @@ number_of_lies = len(lies)
 
 
 def get_lies():
-    dict_of_lies = {}
+    list_of_lies = []
+    
     for item in lies:
         date = item.find('strong').text[0:-1] + ", 2017"
         lie = item.contents[1]
         explanation = item.find('a').text[1:-1]
         evidence = item.find('a')['href']
-        dict_of_lies[date] = lie, explanation, evidence
-        return dict_of_lies
+        list_of_lies.append((date, lie, explanation, evidence))
     
-print(dict_of_lies)
+#    print(list_of_lies)
+    return list_of_lies
+
+list_of_lies = get_lies()
+
+print(list_of_lies)
+
+#print(list_of_lies)
+
     
     
